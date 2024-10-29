527e4a667c1e7ac8af599287413e9444515a58d6
Gowthamvsamy <gowtham.vsamy@gmail.com>
2024-10-29 11:46:20 +0530

    db changes

--- dbConnection.js
+++ dbConnection.js
@@ -6,7 +6,7 @@ const mongoURI = process.env.MONGO_URI;
 
 async function createDbConnection() {
     try {
-        await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(mongoURI);
         console.log('Connected to MongoDB successfully');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error.message);
